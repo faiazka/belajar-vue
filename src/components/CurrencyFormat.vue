8670fda6e713a6bd9e4563bcb4a6d8a079c1449a
Failasuf Azka <failsfzka@gmail.com>
2024-09-30 20:19:30 +0700

    error(Cannot read properties of undefined)

---
+++ src/components/CurrencyFormat.vue
@@ -0,0 +1,15 @@
+<template>
+  <span>{{ value }}</span>
+</template>
+
+<script>
+export default {
+  name: "CurrencyFormat",
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav class="navbar navbar-light fixed-top">
+    <div class="navbar-text ml-auto d-flex">
+      <button
+        class="btn btn-sm btn-outline-success"
+        @click="$emit('toggle-slide')"
+      >
+        <font-awesome-icon icon="dollar-sign"></font-awesome-icon>
+      </button>
+      <div class="dropdown ml-2" v-if="cart.length > 0">
+        <button
+          class="btn btn-success btn-sm dropdown-toggle"
+          id="dropdownCart"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <span class="badge badge-pill badge-light">{{ cartQty }}</span>
+          <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+          <price :value="Number(cartTotal)"></price>
+        </button>
+        <div
+          class="dropdown-menu dropdown-menu-right"
+          aria-labelledby="dropdownCart"
+        >
+          <div v-for="(item, index) in cart" :key="index">
+            <div class="dropdown-item-text text-nowrap text-right">
+              <span class="badge badge-pill badge-warning align-text-top mr-1">
+                {{ item.qty }}
+              </span>
+              {{ item.product.name }}
+              <b><currency-format :value="item.product.price * item.qty" /></b>
+              <a
+                href="#"
+                class="badge badge-danger text-white"
+                @click.stop="$emit('delete-item', index)"
+                >-</a
+              >
+            </div>
+          </div>
+          <router-ling
+            class="btn btn-sm btn-outline-info text-dark float-right mr-2"
+            to="/checkout"
+            >Checkout</router-ling
+          >
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Price from "./Price.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "navbarSegments",
+  props: ["cart", "cartQty", "cartTotal"],
+  components: {
+    Price,
+    FontAwesomeIcon,
+  },
+  filters: {
+    currencyFormat: function (value) {
+      return "Rp" + Number.parseFloat(value).toFixed(2);
+    },
+  },
+};
+</script>
